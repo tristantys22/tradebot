e5b748fbc501839979ab1ffc325d339f2108ab1e
Tristan Tan <tristantan@Tristans-MacBook-Air.local>
2026-04-21 03:11:48 +0800

    add worker for cloud deployment

--- bot_worker.py
+++ bot_worker.py
@@ -1,12 +1,17 @@
 import time
 from ml_trading_bot import sync_subscribers
 
-print("🚀 Bot worker started...")
+print("Bot worker started...")
+
 
 while True:
     try:
         sync_subscribers()
     except Exception as e:
         print(f"[Worker Error] {e}")
+<<<<<<< HEAD
 
     time.sleep(5)  # check Telegram every 5 seconds
+=======
+    time.sleep(5)
+>>>>>>> cb79663 (add worker for cloud deployment)
