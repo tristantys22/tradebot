cfe01c60976128548831844fc6d9b4dbf05814cf
Tristan Tan <tristantan@Tristans-MacBook-Air.local>
2026-04-21 03:36:30 +0800

    fix merge conflict in worker

--- bot_worker.py
+++ bot_worker.py
@@ -9,9 +9,6 @@ while True:
         sync_subscribers()
     except Exception as e:
         print(f"[Worker Error] {e}")
-<<<<<<< HEAD
 
     time.sleep(5)  # check Telegram every 5 seconds
-=======
-    time.sleep(5)
->>>>>>> cb79663 (add worker for cloud deployment)
+    
